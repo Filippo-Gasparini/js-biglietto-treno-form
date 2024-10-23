e93a22dbb439b7a0062e0b1a02bcd1a9b6965886
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-23 16:49:35 +0200

    ADD: correzione

--- app.js
+++ app.js
@@ -14,31 +14,42 @@ console.log('bigietto treno')
 
 
 // km che vuole percorrere l'utente
-const km = document.getElementById('Inserisci i km')
+const km = document.getElementById('km')
 // età dell'utente
-const userAge = document.getElementById('Inserisci la tua età')
+const userAge = document.getElementById('eta')
 
 
+const nome = document.getElementById('nomecognome')
+
+
+const bottonegenera = document.getElementById('genera')
+console.log(bottonegenera)
 
 //vado ad applicare la formula per lo sconto per i minorenni e over 65
 
 
-//biglietto scontato per minorenni
-
-const genera = ''
-if (userAge < 18) {
-    sconto = (km * 20) / 100;
-
-//biglietto scontato per gli over 65
-
-} else if (userAge > 65) {
-    sconto = (km * 40) / 100;
-}
-
 //vado a generare il prezzo del biglietto
 
-genera.addEventListener ("click",function(){
-    console.log()
+bottonegenera.addEventListener("click", function (event) {
+    event.preventDefault()
+    
+    const nomeInput = nome.value;
+    const kmInput = km.value
+    const userAgeInput = userAge.value
+
+
+
+    //biglietto scontato per minorenni
+    if (userAge < 18) {
+        sconto = (km * 20) / 100;
+
+        //biglietto scontato per gli over 65
+
+    } else if (userAge > 65) {
+        sconto = (km * 40) / 100;
+    }
+
+    console.log(bottonegenera)
 })
 
 
